f134e9929528406ed7d9abbfa5209604505ca6ba
Your MujtabaSultan <b.mojtabaalilol@gmail.com>
2025-11-29 11:40:45 +0300

    finding the longest substring is it the

--- src/com/hw/labda/CommonPrefix.java
+++ src/com/hw/labda/CommonPrefix.java
@@ -6,19 +6,18 @@ public class CommonPrefix {
 
     public static String longestCommonPrefix(String[] words) {
         String ans="";
-        int lastMax=0;
+       // int lastMax=0;
         String current ="";
         String [] firstWord = words[0].split("");
         for (int i = 0; i <firstWord.length ; i++) {
             current+=firstWord[i];
             String loopCurr = current;
            if(Arrays.stream(words)
-                   .allMatch(e->e.contains(loopCurr))&&current.length()>ans.length()){
-               ans=current;
+                   .allMatch(e->e.contains(loopCurr))){
+               if(current.length()>ans.length())ans=current;
            }
            else {
-               current=null;
-               break;
+               current="";
            }
 
         }
@@ -28,7 +27,7 @@ public class CommonPrefix {
     }
 
     public static void main(String[] args) {
-        System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+        System.out.println(longestCommonPrefix(new String[]{"flower", "flower", "flightwer"}));
         System.out.println(longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
     }
 
